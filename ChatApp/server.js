0a2ac0dc5e1918024e68b6d095b7070e39d22a1a
Ross Patiak <ross.patiak@gmail.com>
2021-04-13 17:50:28 -0400

    slight changes to chat app

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -2,12 +2,12 @@ let app = require("express")();
 let http = require("http").Server(app);   // to load the library we have run port number using hhtp module 
 let io = require("socket.io")(http);
 
+
 app.get("/",(req,res)=> {
     res.sendFile(__dirname+"/index.html");
-})
+});
 
 io.on("connection",(socket)=> {
-    console.log("Client connected to application.....");
     
     socket.on("chat", packet => {
         let { name, msg } = packet;
@@ -16,5 +16,6 @@ io.on("connection",(socket)=> {
         console.log(`Your message is: ${ msg }`);
 
     })
-})
-http.listen(9090,()=>console.log('server running on port number 9090'));
+});
+
+http.listen(9090, () => console.log('Server running on port number 9090'));
